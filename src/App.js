b1e1f6d645bba4c83dc7c1465185bb5c943622f9
MariusBaldovin <youremailid@example.com>
2023-09-02 07:24:12 +0100

    created  react js SPA structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import OfferRide from "./components/OfferRide";
+import Rewards from "./components/Rewards";
+import FAQ from "./components/FAQ";
 
 const App = () => {
   return (
-    <>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/OfferRide" element={<OfferRide />} />
+      <Route exact path="/Rewards" element={<Rewards />} />
+      <Route exact path="/FAQ" element={<FAQ />} />
+    </Routes>
   );
 };
 export default App;
---
+++ src/components/OfferRide.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const OfferRide = () => {
+  return (
+    <>
+      <h1>Offer Ride</h1>
+    </>
+  );
+};
+export default OfferRide;
